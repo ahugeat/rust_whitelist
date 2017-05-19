b61a74bd18109696cb76eb67c172b1d3c8bafe91
ahugeat <hugeat.arthur@gmail.com>
2017-05-19 20:58:32 +0200

    Take the path form command line parameters.

--- src/whitelist-watcher.cc
+++ src/whitelist-watcher.cc
@@ -1,8 +1,19 @@
+#include <iostream>
+
 #include "local/FileMonitor.h"
 
-int main() {
+int main(int argc, char *argv[]) {
+    // If a argument is missing
+    if (argc != 3) {
+        std::cerr << "Error: Missing argument" << std::endl;
+        std::cerr << "Usage:" << std::endl;
+        std::cerr << "\t" << argv[0] << " LOG_DIRECTORY WHITELIST_FILE" << std::endl;
+
+        return -1;
+    }
+
     // Monitoring the log file
-    FileMonitor logMonitor("log/", "whitelist.txt");
+    FileMonitor logMonitor(argv[1], argv[2]);
 
     for (;;) {
         logMonitor.watch();
